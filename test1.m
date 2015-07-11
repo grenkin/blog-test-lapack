591965aa39c5e47c1e445e3e3c6c440ee83605f2
Gleb Grenkin <grenkin@users.noreply.github.com>
2015-07-11 15:43:43 +1000

    Create test1.m

---
+++ test1.m
@@ -0,0 +1,62 @@
+clear all;
+more off;
+format long;
+
+global L = 100;
+global sigma = 0.2;
+K = 0.01;
+
+alpha = 0;
+beta = 1;
+
+M = 2000;
+global h = 2*L/M;
+
+
+function xi = xi(i)
+  global L;
+  global h;
+  xi = -L + (i-1)*h;    
+end
+
+function v = v(alpha, x)
+  global L;
+  v = alpha*(x + L);
+end
+
+function w = w(beta, T)
+  global sigma;
+  w = (T - sigma) * (1 - T) * exp(-beta/T);
+end
+
+  t0 = clock();
+
+  b = zeros(M+1, 1);
+  Told = zeros(M+1, 1);
+  l = u = d = zeros(1, M+1)';
+
+  for step = 1:20
+
+    for i = 1:M+1
+      Told(i) = 1 + (sigma - 1) * (xi(i) + L) / (2*L);
+    end
+
+    d(1) = 1; u(2) = 0;
+    d(M+1) = 1; l(M) = 0;
+    b(1) = 1;
+    b(M+1) = sigma;
+    for i = 2:M
+      l(i-1) = 1/h^2 - v(alpha, xi(i))/(2*h);
+      u(i+1) = 1/h^2 + v(alpha, xi(i))/(2*h);
+      d(i) = -2/h^2;
+      b(i) = -K*w(beta, Told(i));
+    end
+    A = spdiags([l d u], -1:1, M+1, M+1);
+
+    T = A\b;
+
+  end  % for step
+  
+  elapsed_time = etime(clock(), t0);
+  disp("solving time");
+  elapsed_time
